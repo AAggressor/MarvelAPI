23d2a3d83feeae434ced5ec399b89590e943e45a
Aggressor <dehtjar@gmail.com>
2023-02-09 22:57:39 +0200

    livecircle

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,39 +1,58 @@
 // 1.внутри компонента будет какое-то состояние потомучто когда ми будемо робити запрос
 // ці данні потрібно буде десь сохранити у зв'язку з цим ми переробляємо цей компонент у формат класса
 import { Component } from "react";
+import Spinner from "../spinner/Spinner";
+import ErrorMessage from "../errorMessage/ErrorMessage";
 import MarvelService from "../../services/MarvelService";
 import "./randomChar.scss";
 import mjolnir from "../../resources/img/mjolnir.png";
 
 class RandomChar extends Component {
-  constructor(props) {
-    super(props);
-    this.updateChar();
-  }
-
   // 2. наступним кроком ми будемо наше состояние, тута будемо використовувати сінтаксис полей класа,
   // тому конструктор використовувати не будемо.
   state = {
     char: {},
+    loading: true,
+    error: false,
   };
   // 6.ми створили нове свойство внутрі класу RandomChar/
   marvelService = new MarvelService();
+  // для того чтобі во время работі спинера проходила загрузка добовляем ее в метод
+  // ончарлоєдид
+  componentDidMount() {
+    this.updateChar();
+    this.timerId = setInterval(this.updateChar, 5000);
+  }
 
-  onChatLoaded = (char) => {
-    this.setState({ char });
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  onCharLoaded = (char) => {
+    this.setState({ char, loading: false });
+  };
+
+  onError = () => {
+    this.setState({ loading: false, error: true });
   };
 
   // 7. cтворюємо новий метод котрий буде обращаться к серверу получать данні і записувати це в стейт
   updateChar = () => {
     const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-    this.marvelService.getCharacter(id).then(this.onChatLoaded);
+    this.marvelService
+      .getCharacter(id)
+      .then(this.onCharLoaded)
+      .catch(this.onError);
   };
 
   render() {
     //   3. якщо стейт готов то ми його повинні викорористи у методі render/
-    const {
-      char: { name, description, thumbnail, homepage, wiki },
-    } = this.state;
+    const { char, loading, error } = this.state;
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    // Для контента условие звучит так: если у нас нет загрузки и нет ошибки тогда ми вставляем контент.
+    const content = !(loading || error) ? <View char={char} /> : null;
+
     // треба не забувати про this/
     // 4. теперь ми підставляємо наші переменні в нужні места
     //   5. наступна задача отримати ці данні від сервера, записати їх в стейт а потім використи у нашій верстке.
@@ -41,25 +60,9 @@ class RandomChar extends Component {
 
     return (
       <div className="randomchar">
-        <div className="randomchar__block">
-          <img
-            src={thumbnail}
-            alt="Random character"
-            className="randomchar__img"
-          />
-          <div className="randomchar__info">
-            <p className="randomchar__name">{name}</p>
-            <p className="randomchar__descr">{description}</p>
-            <div className="randomchar__btns">
-              <a href={homepage} className="button button__main">
-                <div className="inner">homepage</div>
-              </a>
-              <a href={wiki} className="button button__secondary">
-                <div className="inner">Wiki</div>
-              </a>
-            </div>
-          </div>
-        </div>
+        {errorMessage}
+        {spinner}
+        {content}
         <div className="randomchar__static">
           <p className="randomchar__title">
             Random character for today!
@@ -76,5 +79,26 @@ class RandomChar extends Component {
     );
   }
 }
+// 8. Для того чтобі спинер работал создаем рендерищей компонент
+const View = ({ char }) => {
+  const { name, description, thumbnail, homepage, wiki } = char;
+  return (
+    <div className="randomchar__block">
+      <img src={thumbnail} alt="Random character" className="randomchar__img" />
+      <div className="randomchar__info">
+        <p className="randomchar__name">{name}</p>
+        <p className="randomchar__descr">{description}</p>
+        <div className="randomchar__btns">
+          <a href={homepage} className="button button__main">
+            <div className="inner">homepage</div>
+          </a>
+          <a href={wiki} className="button button__secondary">
+            <div className="inner">Wiki</div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default RandomChar;
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -27,9 +27,21 @@ class MarvelService {
 
   // создаем метод которий включає функцию по вичлению данних яку ми переносимо сюди з RandomChar/
   _transformCharacter = (char) => {
+    // МОЕ РЕШЕНИЕ
+    // let desc = () => {
+    //   if (char.description.length === 0) {
+    //     return "now and again";
+    //   } else if (char.description.length > 25) {
+    //     return char.description.substring(0, 25) + "...";
+    //   } else {
+    //     return char.description;
+    //   }
+    // };
     return {
       name: char.name,
-      description: char.description,
+      description: char.description
+        ? `${char.description.slice(0, 210)}...`
+        : "There is no description for this character",
       thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
       homepage: char.urls[0].url,
       wiki: char.urls[1].url,
